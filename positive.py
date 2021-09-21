e2e1f15067b0c61c023b2aa21b028b374f3ce9b3
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2021-09-21 19:12:22 +0530

    created positive num.py
    this file displays positive numbers in a given list

---
+++ positive.py
@@ -0,0 +1,6 @@
+#printing positive numbers from given lists
+mylist = [12, -4, 52, -53, 98, -42, -5]
+#to print only positive numbers i am using for loop
+for x in mylist:
+    if x >= 0:
+        print(x)
